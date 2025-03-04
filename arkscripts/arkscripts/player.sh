7a45ed5b766b60aa28cd68d4093acae269a2bdae
Arkboi X <arkboi.x@gmail.com>
2025-03-04 17:22:07 +0530

    doom colors and waybar

---
+++ arkscripts/arkscripts/player.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+get_metadata() {
+    playerctl metadata --format '{"text": "🎵 {{artist}} - {{title}}", "tooltip": "{{album}}"}' 2>/dev/null || echo '{"text": "🎵 Not Playing", "tooltip": "No media playing"}'
+}
+
+# Send initial output
+get_metadata
+
+# Listen for player changes and update Waybar
+playerctl -a metadata --format '{"text": "🎵 {{artist}} - {{title}}", "tooltip": "{{album}}"}' --follow | while read -r line; do
+    echo "$line" > /tmp/playerctl-output.json  # Debugging step
+    pkill -SIGRTMIN+10 waybar
+done
--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c"
+   '("56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700"
+     "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c"
      "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
