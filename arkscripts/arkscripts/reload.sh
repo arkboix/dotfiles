b196284804fb2e47efa06f9c413855015fd5d68d
Arkboi X <arkboi.x@gmail.com>
2025-03-07 19:46:13 +0530

    some updates

--- arkscripts/arkscripts/reload.sh
+++ arkscripts/arkscripts/reload.sh
@@ -5,11 +5,15 @@
 notify-send "Reloading"
 
 # Waybar
-waybar -c ~/.config/waybar/theme-3/config.jsonc -s ~/.config/waybar/theme-3/style.css
+waybar
 sleep 2
 # Mako
 pkill mako
 mako &
+
+# NWG Wrapper
+pkill nwg-wrapper
+nwg-wrapper -t bindings.pango -c bindings.css -p left -a end -mb 30 -ml 30
 
 # Notifs
 notify-send -t 2000 "Reloaded"
