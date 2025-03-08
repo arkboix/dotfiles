e35e765260cea2241bf3e304e4743a370a61600d
Arkboi X <arkboi.x@gmail.com>
2025-03-08 08:03:30 +0530

    mornin' commit

--- arkscripts/arkscripts/wal.sh
+++ arkscripts/arkscripts/wal.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env sh
 # Arkscripts - https://github.com/arkboix/dotfiles
 SELECTED_WALL="$(swww query | awk '{print $8}')"
-ln -s ~/.cache/wal/colors-waybar.css ~/.config/waybar/pywal.css
-ln -s ~/.cache/wal/colors-waybar.css ~/.config/waybar/theme-2/pywal.css
-ln -s ~/.cache/wal/colors-waybar.css ~/.config/waybar/theme-3/pywal.css
-ln -s ~/.cache/wal/colors-waybar.css ~/.config/waybar/theme-4/pywal.css
-ln -s ~/.cache/wal/colors-waybar.css ~/.config/waybar/theme-5/pywal.css
-ln -s ~/.cache/wal/colors-waybar.css ~/.config/waybar/theme-6/pywal.css
+rm ~/.config/waybar/pywal.css
+rm ~/.config/waybar/theme-2/pywal.css
+rm ~/.config/waybar/theme-3/pywal.css
+rm ~/.config/waybar/theme-4/pywal.css
+rm ~/.config/waybar/theme-5/pywal.css
+rm ~/.config/waybar/theme-6/pywal.css
+
+touch ~/.config/waybar/pywal.css
+touch ~/.config/waybar/theme-2/pywal.css
+touch ~/.config/waybar/theme-3/pywal.css
+touch ~/.config/waybar/theme-4/pywal.css
+touch ~/.config/waybar/theme-5/pywal.css
+touch ~/.config/waybar/theme-6/pywal.css
 
 wal -i "$SELECTED_WALL"
+
+cat ~/.cache/wal/colors-waybar.css > ~/.config/waybar/pywal.css
+cat ~/.cache/wal/colors-waybar.css > ~/.config/waybar/theme-2/pywal.css
+cat ~/.cache/wal/colors-waybar.css > ~/.config/waybar/theme-3/pywal.css
+cat ~/.cache/wal/colors-waybar.css > ~/.config/waybar/theme-4/pywal.css
+cat ~/.cache/wal/colors-waybar.css > ~/.config/waybar/theme-5/pywal.css
+cat ~/.cache/wal/colors-waybar.css > ~/.config/waybar/theme-6/pywal.css
